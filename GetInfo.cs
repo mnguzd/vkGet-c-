26ba1a01f53b8c00aa36ee6a2b010a8a23710b68
mnguzd <roman.kioroglo@gmail.com>
2020-01-29 23:54:39 +0200

    fourth commit

---
+++ GetInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VKgetter
+{
+    class GetInfo
+    {
+        public GetInfo(string Uri)
+        {
+
+
+
+        }
+        private static string ServiceKey = "0f6157a20f6157a20f6157a2770f0e069300f610f6157a2514d9ff41e76ca2d8a3413ad";
+        private string NumberOfFriends { get; set; }
+        private string Name { get; set; }
+        private string Surname { get; set; }
+        private string Age { get; set; }
+
+
+
+
+
+
+    }
+}
